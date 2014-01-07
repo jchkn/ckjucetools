6632ee87c441355d934b1bb29c814f0bdeaa5521
jchkn <christian@knufinke.de>
2014-01-07 03:23:08 -0800

    update initial modification time

--- Containers/MultiProcessPropertiesFileWrapper.h
+++ Containers/MultiProcessPropertiesFileWrapper.h
@@ -19,7 +19,7 @@ public:
 	MultiProcessPropertiesFileWrapper(PropertiesFile * propertiesFileNonOwned)
 		: propertiesFile(propertiesFileNonOwned)
 	{
-		
+			updateModificationTime();
 	};
 
 	virtual ~MultiProcessPropertiesFileWrapper()
